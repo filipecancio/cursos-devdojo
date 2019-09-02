30f4d9b10915125aba9bb369c3d8d47531cbf18d
Filipe Câncio <filipecancio.qti@gmail.com>
2019-09-02 12:05:55 -0300

    Maratona JSF - Aula 07: Expression Language pt 04, Coleções https://www.youtube.com/watch?v=3wkzfrtIMVA&list=PL62G310vn6nHSNpACkELWiPlM8J8z8t5J&index=8

--- maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
+++ maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
@@ -5,10 +5,15 @@ import br.com.devdojo.maratonajsf.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Named
 public class EstudanteRegistrarBean  implements Serializable {
-    Estudante estudante = new Estudante();
+    private Estudante estudante = new Estudante();
+    private String[] nome = {"Joana","Marcos","Tânia","Fernando","Dalila"};
+    private List<String> nomeList = asList("Joana","Marcos","Tânia","Fernando","Dalila");
 
     public Estudante getEstudante() {
         return estudante;
@@ -16,5 +21,21 @@ public class EstudanteRegistrarBean  implements Serializable {
 
     public void setEstudante(Estudante estudante) {
         this.estudante = estudante;
+    }
+
+    public String[] getNome() {
+        return nome;
+    }
+
+    public void setNome(String[] nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getNomeList() {
+        return nomeList;
+    }
+
+    public void setNomeList(List<String> nomeList) {
+        this.nomeList = nomeList;
     }
 }
