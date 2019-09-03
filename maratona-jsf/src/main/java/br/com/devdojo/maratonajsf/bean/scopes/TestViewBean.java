90c994cfd11332f1782d30b7c3cb1d8111e4d748
Filipe Câncio <filipecancio.qti@gmail.com>
2019-09-03 13:17:08 -0300

    Maratona JSF - Aula 14: Escopos pt 03, ViewScoped
     https://www.youtube.com/watch?v=KzqL1FdzHEQ&list=PL62G310vn6nHSNpACkELWiPlM8J8z8t5J&index=15

--- maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/scopes/TestViewBean.java
+++ maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/scopes/TestViewBean.java
@@ -1,4 +1,39 @@
 package br.com.devdojo.maratonajsf.bean.scopes;
 
-public class TestViewBean {
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.Arrays.asList;
+
+@Named
+@ViewScoped
+public class TestViewBean implements Serializable {
+    private String nome = "Maratona JSF - Aula 14: Escopos pt 03, ViewScoped";
+    private List<String> oferta = asList("Amostra Avon","Amostra OBoticário","Amostra Natura");
+    private List<String> ofertaSelecionado = new ArrayList<>();
+
+    public void  selecionarOferta(){
+        int index = ThreadLocalRandom.current().nextInt(3);
+        String off = oferta.get(index);
+        ofertaSelecionado.add(off);
+    }
+    public List<String> getOfertaSelecionado() {
+        return ofertaSelecionado;
+    }
+
+    public void setOfertaSelecionado(List<String> ofertaSelecionado) {
+        this.ofertaSelecionado = ofertaSelecionado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
