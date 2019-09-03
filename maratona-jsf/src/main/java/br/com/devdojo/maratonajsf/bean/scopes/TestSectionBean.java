bd42f049781d3be469862e46c0f61e85c4d86173
Filipe Câncio <filipecancio.qti@gmail.com>
2019-09-03 12:49:26 -0300

    Maratona JSF - Aula 13: Escopos pt 02, SessionScoped https://www.youtube.com/watch?v=Rj6DxZ2e6zk&list=PL62G310vn6nHSNpACkELWiPlM8J8z8t5J&index=14

--- maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/scopes/TestSectionBean.java
+++ maratona-jsf/src/main/java/br/com/devdojo/maratonajsf/bean/scopes/TestSectionBean.java
@@ -1,6 +1,7 @@
 package br.com.devdojo.maratonajsf.bean.scopes;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,16 +11,20 @@ import java.util.concurrent.ThreadLocalRandom;
 import static java.util.Arrays.asList;
 
 @Named
-@RequestScoped
-public class TestRequestBean implements Serializable {
-    private String nome = "Maratona JSF - Aula 12: Escopos pt 01, RequestScoped";
-    private List<String> ofertas = asList("10 reais","20 reais","40 reais");
+@SessionScoped
+public class TestSectionBean implements Serializable {
+    private String nome = "Maratona JSF - Aula 13: Escopos pt 02, SessionScoped";
+    private List<String> oferta = asList("40% de desconto.","10x sem juros.","primeira parcela grátis.");
     private List<String> ofertaSelecionado = new ArrayList<>();
 
     public void  selecionarOferta(){
         int index = ThreadLocalRandom.current().nextInt(3);
-        String personagem = ofertas.get(index);
-        ofertaSelecionado.add(personagem);
+        String off = oferta.get(index);
+        ofertaSelecionado.add(off);
+    }
+    public String logout(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "page01?faces-redirect=true";
     }
 
     public List<String> getOfertaSelecionado() {
